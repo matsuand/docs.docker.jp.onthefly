2095b55940010c3556f4fed967c3d430f96e3ae0
matsuand <30614168+matsuand@users.noreply.github.com>
2020-03-23 16:42:31 +0900

    サイドバーリンク不備修正。

--- js/docs.js
+++ js/docs.js
@@ -77,7 +77,7 @@ function walkTree(tree) {
             outputLetNav.push("</ul></li>");
         } else {
             // just a regular old topic; this is a leaf, not a branch; render a link!
-            outputLetNav.push('<li><a href="' + tree[j].path + '"')
+            outputLetNav.push('<li><a href="/docs.docker.jp.onthefly' + tree[j].path + '"')
             if (tree[j].path === pageURL && !tree[j].nosync) {
                 sectionToHighlight = currentSection;
                 outputLetNav.push('class="active currentPage"')
