012651f444a72ac3d1500860aed9166e84a337d8
matsuand <30614168+matsuand@users.noreply.github.com>
2020-10-15 15:52:57 +0900

    latest/0d28c3cb14 対応。出力HTMLコミット。(関連ページ全コミット。)

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -152,45 +152,8 @@ $(window).scroll(function () {
     }
 });
 
-
-// Cookie functions
-function createCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === " ") c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-var prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-var selectedNightTheme = readCookie("night");
-
-if (selectedNightTheme === "true" || (selectedNightTheme === null && prefersDark)) {
-    applyNight();
-    $("#switch-style").prop("checked", true);
-} else {
-    applyDay();
-    $("#switch-style").prop("checked", false);
-}
-
-
 /*
- *
- * toggle menu *********************************************************************
- *
+ * toggle menu *****************************************************************
  */
 
 $("#menu-toggle").click(function (e) {
@@ -268,11 +231,8 @@ $(document).ready(function () {
     });
 });
 
-
 /*
- *
- * make dropdown show on hover *********************************************************************
- *
+ * make dropdown show on hover *************************************************
  */
 
 $("ul.nav li.dropdown").hover(function () {
@@ -282,50 +242,7 @@ $("ul.nav li.dropdown").hover(function () {
 });
 
 /*
- *
- * swapStyleSheet*********************************************************************
- *
- */
-
-function applyNight() {
-    $("body").addClass("night");
-}
-
-function applyDay() {
-    $("body").removeClass("night");
-}
-
-$("#switch-style").change(function () {
-    if ($(this).is(":checked")) {
-        applyNight();
-        createCookie("night", true, 999)
-    } else {
-        applyDay();
-        createCookie("night", false, 999);
-    }
-});
-
-
-/*
- *
- * TEMP HACK For side menu*********************************************************************
- *
- */
-
-$(".nav-sidebar ul li a").click(function () {
-    $(this).addClass("collapse").siblings().toggleClass("in");
-});
-
-if ($(".nav-sidebar ul a.active").length !== 0) {
-    $(".nav-sidebar ul").click(function () {
-        $(this).addClass("collapse in").siblings;
-    });
-}
-
-/*
- *
- * Components *********************************************************************
- *
+ * Components ******************************************************************
  */
 
 $(function () {
